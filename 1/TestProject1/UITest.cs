20cd57583ab093d1b7736f6d00f5faae64ceefb4
Александра <232909072091-26>
2026-04-23 13:48:47 +0300

    Добавлен файл тестирования для UI элементов формы MealPlanForm.cs

---
+++ 1/TestProject1/UITest.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows.Forms;
+
+namespace RecipeTests
+{
+    [TestClass]
+    public class UITests
+    {
+        private MealPlanForm _mealPlanForm;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _mealPlanForm = new MealPlanForm();
+        }
+
+        [TestMethod]
+        public void ListView_IsVisibleAndEnabled()
+        {
+            // Assert
+            Assert.IsTrue(_mealPlanForm.listView.Visible);
+            Assert.IsTrue(_mealPlanForm.listView.Enabled);
+        }
+
+        [TestMethod]
+        public void AddRecipeButton_IsVisibleAndEnabled()
+        {
+            // Assert
+            Assert.IsTrue(_mealPlanForm.addRecipeButton.Visible);
+            Assert.IsTrue(_mealPlanForm.addRecipeButton.Enabled);
+        }
+
+        [TestMethod]
+        public void RemoveRecipeButton_IsVisibleAndEnabled()
+        {
+            // Assert
+            Assert.IsTrue(_mealPlanForm.removeRecipeButton.Visible);
+            Assert.IsTrue(_mealPlanForm.removeRecipeButton.Enabled);
+        }
+
+        [TestMethod]
+        public void SearchRecipeButton_IsVisibleAndEnabled()
+        {
+            // Assert
+            Assert.IsTrue(_mealPlanForm.searchRecipeButton.Visible);
+            Assert.IsTrue(_mealPlanForm.searchRecipeButton.Enabled);
+        }
+
+        [TestMethod]
+        public void ListView_HasTwoColumns()
+        {
+            // Assert
+            Assert.AreEqual(2, _mealPlanForm.listView.Columns.Count);
+        }
+
+        [TestMethod]
+        public void ListView_FirstColumnHeader_IsDate()
+        {
+            // Assert
+            Assert.AreEqual("Дата", _mealPlanForm.listView.Columns[0].Text);
+        }
+
+        [TestMethod]
+        public void ListView_SecondColumnHeader_IsName()
+        {
+            // Assert
+            Assert.AreEqual("Название", _mealPlanForm.listView.Columns[1].Text);
+        }
+
+        [TestMethod]
+        public void ListView_InitiallyEmpty()
+        {
+            // Assert
+            Assert.AreEqual(0, _mealPlanForm.listView.Items.Count);
+        }
+
+
+        [TestMethod]
+        public void Form_HasCorrectTitle()
+        {
+            // Assert
+            Assert.AreEqual("Планирование меню", _mealPlanForm.Text);
+        }
+    }
+}
