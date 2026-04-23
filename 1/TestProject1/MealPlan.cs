2f7304536eee0fda5db817a1c4d0fe70c7b4836d
Александра <232909072091-26>
2026-04-23 13:38:33 +0300

    Добавлен файл тестрования для класса MealPlan.cs

---
+++ 1/TestProject1/MealPlan.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+
+namespace RecipeTests
+{
+    [TestClass]
+    public class MealPlanTests
+    {
+        private MealPlan _mealPlan;
+        private ListView _listView;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _listView = new ListView();
+            _mealPlan = new MealPlan(_listView);
+        }
+
+        [TestMethod]
+        public void AddRecipeToPlan()
+        {
+            // Arrange
+
+            var mealPlan = new MealPlan(_listView);
+            // Act & Assert
+
+            Assert.IsNotNull(mealPlan);
+        }
+
+        [TestMethod]
+        public void RemoveRecipeFromPlan()
+        {
+            // Arrange
+            _listView.SelectedItems.Clear();
+            var mealPlan = new MealPlan(_listView);
+            // Act
+            mealPlan.RemoveRecipeFromPlan();
+            // Assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void SearchRecipeByName()
+        {
+            // Arrange
+            var mealPlan = new MealPlan(_listView);
+            // Act & Assert
+            Assert.IsNotNull(mealPlan);
+        }
+
+        [TestMethod]
+        public void LoadPlan()
+        {
+            // Arrange
+            _listView.Items.Add("Тестовый элемент");
+            Assert.AreEqual(1, _listView.Items.Count);
+
+            // Act
+            var mealPlan = new MealPlan(_listView);
+
+            // Assert
+            Assert.AreEqual(0, _listView.Items.Count);
+        }
+    }
+}
